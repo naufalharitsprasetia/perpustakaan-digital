2e16c99a40d36b2bb87a798184da7c8078c523c6
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2024-03-18 23:31:29 +0700

    first init

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layout.main')
+
+@section('css')
+    {{-- <link rel="stylesheet" href="/css/contoh.css"> --}}
+@endsection
+
+@section('content')
+    <h1>Hello, world!</h1>
+@endsection
---
+++ resources/views/layout/main.blade.php
@@ -0,0 +1,6 @@
+@include('layout.header')
+@include('layout.navbar')
+<main class="main-container-fluid">
+    @yield('content')
+</main>
+@include('layout.footer')
