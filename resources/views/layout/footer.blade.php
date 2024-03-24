a77859506c346c39201a3fdf1bfde0ec89fc543b
naufalharitsprasetia <naufalharisprasetia@gmail.com>
2024-03-25 04:59:54 +0700

    25 maret 2024 v.2

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,18 +1,45 @@
   <!-- Footer Start -->
   <footer>
       <div class="footer d-flex align-items-center">
-          <div class="logo d-flex align-items-center gap-2">
+          <div class="logo d-flex align-items-center gap-2" style="width: 38rem;">
               <img src="/assets/logo.png" width="100" alt="footer-logo" />
               <h4>Perpustakaan MI PSM Sidorejo</h4>
           </div>
-          <div class="footer-container">
-              <div class="content-contact-alamat d-flex gap-2">
-                  <i class="bi bi-geo-alt-fill"></i>
-                  <p>
-                      Jln.Janglengan Rt/Rw 01/01 Ds., Jangglengan, Sidorejo, Kec. Sukorejo, Kabupaten
-                      Ponorogo, Jawa Timur 63453
-                  </p>
-              </div>
+          <div class="footer-container px-2">
+              <a href="" class="text-decoration-none" style="color:black">
+                  <div class="content-contact-alamat d-flex gap-2">
+                      <i class="bi bi-geo-alt-fill"></i>
+                      <p>
+                          Jln.Janglengan Rt/Rw 01/01 Ds., Jangglengan, Sidorejo, Kec. Sukorejo, Kabupaten
+                          Ponorogo, Jawa Timur 63453
+                      </p>
+                  </div>
+              </a>
+              <a href="" class="text-decoration-none" style="color:black">
+
+                  <div class="content-contact-instagram d-flex gap-2">
+                      <i class="bi bi-instagram"></i>
+                      <p>
+                          Instagram
+                      </p>
+                  </div>
+              </a>
+              <a href="" class="text-decoration-none" style="color:black">
+
+                  <div class="content-contact-facebook d-flex gap-2">
+                      <i class="bi bi-facebook"></i>
+                      <p>
+                          Facebook </p>
+                  </div>
+              </a>
+              <a href="" class="text-decoration-none" style="color:black">
+                  <div class="content-contact-youtube d-flex gap-2">
+                      <i class="bi bi-youtube"></i>
+                      <p>
+                          Youtube </p>
+                  </div>
+              </a>
+
           </div>
       </div>
   </footer>
